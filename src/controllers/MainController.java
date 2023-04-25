b4403ac2f8a4f6bc67028b28d0dfc51db1d0eed9
Peter Kiraly <peter.kiral@gmail.com>
2023-04-25 18:44:25 +0200

    2024-04-25 three test

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -32,6 +32,10 @@ public class MainController {
     public double calcWeight(double perimeter, double length){
         double weight = (perimeter * perimeter * length) / 11877;
         return weight;
+        // 30 * 35 = 2.6521848951755493
+        // 130 * 135 = 192.0939631220005
+        // 150 * 160 = 303.1068451629199
+        // return 2.6521848951755493;
     }
 
 }
--- test/TestWeight.java
+++ test/TestWeight.java
@@ -1,20 +1,43 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import controllers.MainController;
 
 public class TestWeight {
+
+    // Azért, hogy a definíció meglegyeb minden metódus részére
+    MainController mc;
+    
+    // minden egyes teszt előtt..
+    @BeforeEach
+    public void initEach(){
+        mc = new MainController();
+
+    }
     
     // Első teszt
     @Test //anotáció, az alatta lévő metódusra van hatása
-    public void testCalcWeight01(){
-        MainController mc = new MainController();
+    public void testCalcWeight01(){       
         double actual = mc.calcWeight(30, 35);
         double expected = 2.6521848951755493;
         assertEquals(expected, actual);
 
     }
 
+    @Test
+    public void testCalcWeight02(){;
+        double actual = mc.calcWeight(130, 135);
+        double expected = 192.0939631220005;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCalcWeight03(){
+        double actual = mc.calcWeight(150, 160);
+        double expected = 303.1068451629199;
+        assertEquals(expected, actual);
+    }
    
 }
