73b9caa8523bd9d476f7f238ba4fb286c0a5b470
Peter Kiraly <peter.kiral@gmail.com>
2023-04-25 18:08:43 +0200

    2023-04-25 first

--- test/TestWeight.java
+++ test/TestWeight.java
@@ -4,9 +4,16 @@ import org.junit.jupiter.api.Test;
 
 public class TestWeight {
     
+    // Első teszt
     @Test //anotáció, az alatta lévő metódusra van hatása
     public void testValami(){
         assertEquals(1, 1);
 
+    }
+
+    // Második tesztünk
+    @Test
+    public void testValami2(){
+        assertEquals(1, 1);
     }
 }
